b83e0037520ce89f80c3fe6fd03a71c673590564
xxiaoW <wl1223171@outlook.com>
2023-06-21 12:52:52 +0800

    创建

---
+++ craco.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const resolve = dir => path.resolve(__dirname, dir)
+const CracoLessPlugin = require("craco-less")
+
+module.exports = {
+  // less
+  plugins: [{
+    plugin: CracoLessPlugin,
+    // options: {
+    //   cssLoaderOptions: {
+    //     modules: { localIdentName: "[local]_[hash:base64:5]" },
+    //   }
+    // }
+  }],
+  // plugins: [{
+  //   plugin: CracoLessPlugin,
+  //   // 适配css Modules
+  //   options: {
+  //     cssLoaderOptions: {
+  //       module: {
+  //         localIdentName: "[local]_[hash:base64:5]"
+  //       }
+  //     }
+  //   }
+  // }],
+  // webpack
+  webpack: {
+    alias: {  // 配置路径别名
+      "@": resolve("src"),
+      'components': resolve("src/components"),
+    }
+  }
+}
+
---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import appStyle from './App.module.less'
+
+function App() {
+  return (
+    <div className={appStyle.color}>
+      App
+    </div>
+  );
+}
+
+export default App;
